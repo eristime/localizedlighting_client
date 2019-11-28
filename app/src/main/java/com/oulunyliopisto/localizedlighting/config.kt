c9614675de011082c7b2daacf633ad8c30038ea6
Eemeli Ristimella <eemeli.ristimella@gmail.com>
2019-11-28 17:27:25 +0200

    dev key modified

--- app/src/main/java/com/oulunyliopisto/localizedlighting/config.kt
+++ app/src/main/java/com/oulunyliopisto/localizedlighting/config.kt
@@ -5,4 +5,4 @@ const val SERVICE_INTENT_SWITCH_ID = "switchId"
 const val MIME_TEXT_PLAIN = "text/plain"
 
 // THIS NEEDS TO BE THE SAME AS "CLIENT_SECRET" IN SERVER CONFIGURATION FILE
-const val clientToken = "<REPLACE THIS WITH CLIENT_SECRET>"
+const val clientToken = "REPLACE_THIS_DEV_KEY"
